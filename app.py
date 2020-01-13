c250a44efa0079068ba5721d804f3e6092de726c
JP Marin <solidsnake2015@hotmail.com>
2020-01-13 11:25:19 -0400

    files 2

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
+import os
 from flask import Flask
-from flask_restful import  Api
+from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate, identity
-from  resources.user import UserRegister
-from  resources.item import Item,ItemList
-from  resources.store import Store, StoreList
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 from db import db
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'key1'
 api = Api(app)
+
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+
 jwt = JWT(app, authenticate, identity)
 
-api.add_resource(Store,'/store/<string:name>')
-api.add_resource(Item,'/item/<string:name>')
-api.add_resource(ItemList,'/item')
-api.add_resource(StoreList,'/stores')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/item')
+api.add_resource(StoreList, '/stores')
 
-api.add_resource(UserRegister,'/register')
-
+api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
